4e0c2243af4b6b07f530c73e6c0aa612bab9bf2e
wangguangjun <550234256@qq.com>
2016-10-06 19:44:13 +0800

    11

--- dangdang-web/src/main/java/com/wang/serivces/UserServicesImpl.java
+++ dangdang-web/src/main/java/com/wang/serivces/UserServicesImpl.java
@@ -18,10 +18,10 @@ public class UserServicesImpl implements UserServices {
     @Autowired
     private UserMapper userMapper;
     //查询单个
-    public int check(String name, String pwd) {
-        Map map=new HashMap();
-        map.put("userName",name);
-        map.put("passWord",pwd);
+    public int check(String userName, String passWord) {
+        Map<String,String> map=new HashMap<String,String>();
+        map.put("userName",userName);
+        map.put("passWord",passWord);
      List<User> list= userMapper.searchUserByParams(map);
         if(list.size()!=0){
           return list.size();
