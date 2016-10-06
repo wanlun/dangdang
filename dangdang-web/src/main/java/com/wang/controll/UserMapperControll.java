0a0cab736835ecce09d91d95fdc1c2cdcc2de034
wangguangjun <550234256@qq.com>
2016-10-06 18:02:21 +0800

    11

--- dangdang-web/src/main/java/com/wang/controll/UserMapperControll.java
+++ dangdang-web/src/main/java/com/wang/controll/UserMapperControll.java
@@ -1,7 +1,25 @@
 package com.wang.controll;
 
+import com.wang.serivces.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by Administrator on 2016/10/6.
  */
+@Controller
 public class UserMapperControll {
+   @Autowired
+   private UserServices userServices;
+
+
+    @RequestMapping("/check")
+    @ResponseBody
+    public int check(@RequestParam("userName") String userName, @RequestParam("passWord") String passWord){
+        int s= userServices.check(userName,passWord);
+        return s;
+    }
 }
