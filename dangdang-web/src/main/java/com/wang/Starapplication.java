df2a5cefea074e44a3f4a77d46624446424ee645
wangguangjun <550234256@qq.com>
2016-10-06 18:54:11 +0800

    11

--- dangdang-web/src/main/java/com/wang/Starapplication.java
+++ dangdang-web/src/main/java/com/wang/Starapplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
  * Created by Administrator on 2016/10/6.
  */
 @SpringBootApplication
-@ComponentScan({"com.fc","com.wang"})
+@ComponentScan({"com.fc","com.wang.controll","com.wang.serivces"})
 @MapperScan("com.wang.mapper")
 public class Starapplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
--- dangdang-web/src/test/java/DangdangTest.java
+++ dangdang-web/src/test/java/DangdangTest.java
@@ -1,11 +1,23 @@
+import com.wang.Starapplication;
 import com.wang.serivces.BooksServices;
+import com.wang.serivces.UserServices;
+import com.wang.serivces.UserServicesImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by Administrator on 2016/10/6.
  */
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(value= Starapplication.class)
 public class DangdangTest {
      @Autowired
-      BooksServices booksServices;
+      private UserServicesImpl  userServicesImpl;
+    @Test
+      public void  one(){
+
+      }
 }
