b6bb2a2598b786ba6fb1cdabd8fc5c574c0f8c53
Administrator <939124182@qq.com>
2016-10-06 17:30:13 +0800

    uuu

---
+++ dangdang-web/src/main/java/com/wang/Starapplication.java
@@ -0,0 +1,20 @@
+package com.wang;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+@SpringBootApplication
+@ComponentScan({"com.fc","com.wang"})
+@MapperScan("com.wang.mapper")
+public class Starapplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        System.out.println("启动类启动了");
+        SpringApplication.run(Starapplication.class,args);
+    }
+}
---
+++ dangdang-web/src/test/java/DangdangTest.java
@@ -0,0 +1,11 @@
+import com.wang.serivces.BooksServices;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+
+public class DangdangTest {
+     @Autowired
+      BooksServices booksServices;
+}
