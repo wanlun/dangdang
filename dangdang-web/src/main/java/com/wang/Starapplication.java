92f629ceb04b2f3a292d9634a1fa8eabad85161c
wangguangjun <550234256@qq.com>
2016-10-07 09:37:35 +0800

    11

--- dangdang-web/src/main/java/com/wang/Starapplication.java
+++ dangdang-web/src/main/java/com/wang/Starapplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * Created by Administrator on 2016/10/6.
  */
+
 @SpringBootApplication
 @ComponentScan({"com.fc","com.wang.controll","com.wang.serivces"})
 @MapperScan("com.wang.mapper")
