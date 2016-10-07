2b758ef29aa68267274e3d8218e8b4d02198fb93
Administrator <939124182@qq.com>
2016-10-07 11:35:29 +0800

    uuu

--- dangdang-web/src/main/java/com/wang/serivces/BooksServices.java
+++ dangdang-web/src/main/java/com/wang/serivces/BooksServices.java
@@ -7,21 +7,22 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * Created by Administrator on 2016/10/6.
  */
-@Service
-public class BooksServices {
-     @Autowired
-      private BookMapper bookMapper;
 
-    /*调用查询全部的方法，传一个Null进去，就是查询全部*/
-    @RequestMapping("/wanlun")
+public class BooksServices {
+     @Resource
+      private BookMapper bookMapper;
+/*
+    *//*调用*//*@RequestMapping("/wanlun")
     public  String findall(ModelMap modelMap){
          List<Book>  data=bookMapper.searchBookByParams(null);
           modelMap.put("data",data);
         return  "findallbooks";
-    }
+    }*//*查询全部的方法，传一个Null进去，就是查询全部*/
+
 }
--- dangdang-web/src/test/java/DangdangTest.java
+++ dangdang-web/src/test/java/DangdangTest.java
@@ -1,4 +1,4 @@
-import com.wang.Starapplication;
+
 
 import com.wang.serivces.UserServicesImpl;
 import org.junit.Test;
@@ -11,8 +11,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by Administrator on 2016/10/6.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(value= Starapplication.class)
-public class DangdangTest {
+/*@SpringApplicationConfiguration(value= Starapplication.class)*/
+    public class DangdangTest {
      @Autowired
       private UserServicesImpl  userServicesImpl;
     @Test
