bafc27c3aa631c7f54fcf76af7bc3dd7cc719039
wangguangjun <550234256@qq.com>
2016-10-07 09:34:38 +0800

    11

--- dangdang-web/src/main/java/com/wang/Starapplication.java
+++ dangdang-web/src/main/java/com/wang/Starapplication.java
@@ -13,8 +13,9 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"com.fc","com.wang.controll","com.wang.serivces"})
 @MapperScan("com.wang.mapper")
 public class Starapplication extends SpringBootServletInitializer {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("启动类启动了");
         SpringApplication.run(Starapplication.class,args);
+
     }
 }
